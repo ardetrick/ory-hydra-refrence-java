c6194f6e63b38d5c8a09d9dbc5d504e41bdb9f8c
ardetrick <alex.detrick@gmail.com>
2023-07-02 20:37:17 -0500

    ok

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-//    id("org.gradle.toolchains.foojay-resolver-convention")
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 
 rootProject.name = "project-conventions"
